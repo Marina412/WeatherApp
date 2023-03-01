fea3a106362d90058b02dd9e6b2ad1406bc26307
Marina <marinaatef412@gmail.com>
2023-03-01 18:48:29 +0200

    hourly from api

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -5,12 +5,18 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AlertDialog
-import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.weatherapp.Alarts.View.AlertsFragment
+import com.example.weatherapp.Fav.View.FavoriteFragment
+import com.example.weatherapp.Home.View.HomeFragment
 import com.example.weatherapp.Home.ViewModel.HomeViewModel
 import com.example.weatherapp.Home.ViewModel.HomeViewModelFactory
+import com.example.weatherapp.Settings.View.SettingsFragment
+import com.example.weatherapp.Splash.SplashFragment
 import com.example.weatherapp.databinding.ActivityMainBinding
 import com.example.weatherapp.databinding.FragmentHomeBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,17 +24,32 @@ class MainActivity : AppCompatActivity() {
         override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-         binding=DataBindingUtil
-            .setContentView(this,R.layout.activity_main)
-        binding.lifecycleOwner=this
+            binding = ActivityMainBinding.inflate(layoutInflater)
+
+            setContentView(binding.root)
+
+
+
 //       val customDialog=CustomDialog(this).customLocationDialog()//.showOptionDialog()
 //        customDialog.show()
+            navgation(SplashFragment())
+            binding.bottomNavigationView.setOnItemSelectedListener {
+                when(it.itemId){
+                    R.id.home->navgation(HomeFragment())
+                    R.id.settings->navgation(SettingsFragment())
+                    R.id.favorite->navgation(FavoriteFragment())
+                    R.id.alerts->navgation(AlertsFragment())
 
-
-
-
+                }
+                true
+            }
     }
 
-
+    fun navgation(fragment: Fragment){
+        val fragmentManger=supportFragmentManager
+        val fragmentTransition=fragmentManger.beginTransaction()
+        fragmentTransition.replace(R.id.nav_host_fragment,fragment)
+        fragmentTransition.commit()
+    }
 
 }
