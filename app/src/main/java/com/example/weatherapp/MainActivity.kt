318124281dc7859b1e880098853b9653165fc55a
Marina <marinaatef412@gmail.com>
2023-03-05 19:57:12 +0200

    room fav model

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -2,21 +2,13 @@ package com.example.weatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.View
-import androidx.appcompat.app.AlertDialog
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.example.weatherapp.Alarts.View.AlertsFragment
 import com.example.weatherapp.Fav.View.FavoriteFragment
 import com.example.weatherapp.Home.View.HomeFragment
-import com.example.weatherapp.Home.ViewModel.HomeViewModel
-import com.example.weatherapp.Home.ViewModel.HomeViewModelFactory
 import com.example.weatherapp.Settings.View.SettingsFragment
-import com.example.weatherapp.Splash.SplashFragment
 import com.example.weatherapp.databinding.ActivityMainBinding
-import com.example.weatherapp.databinding.FragmentHomeBinding
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,11 +20,11 @@ class MainActivity : AppCompatActivity() {
 
             setContentView(binding.root)
 
-
+Toast.makeText(this,"bbbbbb",Toast.LENGTH_SHORT).show()
 
 //       val customDialog=CustomDialog(this).customLocationDialog()//.showOptionDialog()
 //        customDialog.show()
-            navgation(SplashFragment())
+            navgation(HomeFragment())
             binding.bottomNavigationView.setOnItemSelectedListener {
                 when(it.itemId){
                     R.id.home->navgation(HomeFragment())
